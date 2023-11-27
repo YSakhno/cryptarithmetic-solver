19c28faebad3c68c6b3cfdefa613392f772581bb
Yuri Sakhno <ysakhno@gmail.com>
2023-11-27 13:42:49 -0800

    Changes to allow entering multiple puzzles through standard input
    +a file with a small set of sample puzzles

--- src/main/kotlin/io/ysakhno/puzzles/cryptarithmetic/App.kt
+++ src/main/kotlin/io/ysakhno/puzzles/cryptarithmetic/App.kt
@@ -1,11 +1,19 @@
 package io.ysakhno.puzzles.cryptarithmetic
 
+import java.nio.charset.StandardCharsets.UTF_8
+import java.util.Scanner
+
+/** A [Regex] used to determine if the line of text contains only comment (and should be skipped). */
+private val COMMENT_REGEX = "^\\s*#".toRegex()
+
+/** Determines if this character sequence is either blank or a comment, and returns `true` if it is. */
+private fun CharSequence?.isCommentOrBlank() = isNullOrBlank() || COMMENT_REGEX.containsMatchIn(this)
+
 /** The padding to align solutions with the puzzle above in the output. */
 private const val OUTPUT_PADDING = 15
 
-/** Application's main entry point. */
-fun main() {
-    val puzzle = "I + BB = ILL"
+/** Solves a single puzzle reporting the solution(s) to standard output (console or terminal). */
+private fun solveAndReport(puzzle: String) {
     val solutions = solve(puzzle)
     var hasNoSolutions = true
 
@@ -22,5 +30,18 @@ fun main() {
 
     if (hasNoSolutions) {
         println("Puzzle '$puzzle' has no solutions")
+    }
+}
+
+/** Application's main entry point. */
+fun main() {
+    println("Cryptarithmetic puzzle solver by Yuri Sakhno")
+    println("Input the puzzles to solve, one per line:")
+    println()
+
+    Scanner(System.`in`, UTF_8).use { scanner ->
+        while (scanner.hasNextLine()) {
+            scanner.nextLine().takeUnless(CharSequence::isCommentOrBlank)?.let(::solveAndReport)
+        }
     }
 }
